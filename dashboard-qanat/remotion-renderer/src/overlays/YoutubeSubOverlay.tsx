d5aab937097d887e4622f3a0c52e1b1ab78575f1
leonardosalves <leonardo_sa7@hotmail.com>
2026-06-26 00:25:50 -0300

    feat: add hyperframes ai orquestration, prores transparent render and translate youtube outro

--- dashboard-qanat/remotion-renderer/src/overlays/YoutubeSubOverlay.tsx
+++ dashboard-qanat/remotion-renderer/src/overlays/YoutubeSubOverlay.tsx
@@ -166,10 +166,10 @@ export const YoutubeSubOverlay: React.FC<{
             >
               <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
             </svg>
-            <span>Subscribed</span>
+            <span>Inscrito</span>
           </div>
         ) : (
-          <span>Subscribe</span>
+          <span>Inscrever-se</span>
         )}
       </div>
     </div>
